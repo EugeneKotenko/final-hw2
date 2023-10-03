2b2b75cfb1fb146eff307db39bda67f0aa4a4892
EugeneKotenko <134332591+EugeneKotenko@users.noreply.github.com>
2023-10-03 08:47:46 +0300

    update code

--- client/src/components/SmoozieConstructor.jsx
+++ client/src/components/SmoozieConstructor.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../assets/styles/SmoozieConstructor.scss';
+import '../assets/styles/SmoothieConstructor.scss';
 
 const smoozieConstructor = () => {
   const products = [
